83bee4d88fc2703a54fd318ffeda3170a83cfeb4
jiangzy27 <jiangzy27@gmail.com>
2016-11-10 17:17:19 +0800

    异步赋值问题

--- example/question/server_asycdata.js
+++ example/question/server_asycdata.js
@@ -2,22 +2,27 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+//处理静态资源
+app.use(express.static('./'));
 app.post('/getCity',function(req,res){
 
     var data = req.body;//中间件来的
-    if(data.cityId =='1'){
-        res.send({cityName:'beijing'});
-    }else if(data.cityId =='2'){
-        res.send({cityName:'shanghai'});
-    }else if(data.cityId =='3'){
-        res.send({cityName:'hangzhou'});
-    }else if(data.cityId =='4'){
-        res.send({cityName:'wuhan'});
-    }else if(data.cityId =='5'){
-        res.send({cityName:'nanjing'});
-    }else{
-        res.send({cityName:'chongqing'});
-    }
+    setTimeout(function(){
+        if(data.cityId =='1'){
+            res.send({cityName:'beijing'});
+        }else if(data.cityId =='2'){
+            res.send({cityName:'shanghai'});
+        }else if(data.cityId =='3'){
+            res.send({cityName:'hangzhou'});
+        }else if(data.cityId =='4'){
+            res.send({cityName:'wuhan'});
+        }else if(data.cityId =='5'){
+            res.send({cityName:'nanjing'});
+        }else{
+            res.send({cityName:'chongqing'});
+        }
+    },2000);
+
 
 });
 
